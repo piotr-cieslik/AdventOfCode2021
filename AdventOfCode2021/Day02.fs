9e8cbad969bde968bbb245f9a97c812679913e44
Piotr Cieślik <piotr.cieslik@outlook.com>
2022-05-16 14:57:18 +0200

    Finish day 2 part 1

---
+++ AdventOfCode2021/Day02.fs
@@ -0,0 +1,19 @@
+﻿module Day02
+
+let input = System.IO.File.ReadAllLines("Day02.txt")
+
+type Change = {Direction: string; Value: int}
+type Position = {X: int; Y: int}
+
+let part1 () =
+    let initialPosition = {X=0; Y=0}
+    input
+    |> Array.map (fun change -> change.Split(" "))
+    |> Array.map (fun change -> { Direction = change[0]; Value = int change[1] }) // Split to direction and value
+    |> Array.fold (fun position change ->
+        match change.Direction with
+            | "forward" -> {X=position.X + change.Value; Y=position.Y}
+            | "down" -> {X=position.X; Y=position.Y + change.Value}
+            | "up" -> {X=position.X; Y=position.Y - change.Value}
+            | _ -> {X=position.X; Y=position.Y}) initialPosition
+    |> (fun position -> position.X * position.Y)
--- AdventOfCode2021/Program.fs
+++ AdventOfCode2021/Program.fs
@@ -1,5 +1,10 @@
-﻿let d1p1 = Day01.part1()
+﻿// Day 1
+let d1p1 = Day01.part1()
 printfn "Day 1, part 1: %i" d1p1
 
 let d1p2 = Day01.part2()
 printfn "Day 1, part 2: %i" d1p2
+
+// Day 2
+let d2p1 = Day02.part1()
+printfn "Day 2, part 1: %i" d2p1
