a9adfeb3b0fe247060cf3843444258e3f4d32556
Piotr Cieślik <piotr.cieslik@outlook.com>
2022-05-17 18:17:54 +0200

    Add playground to the project
    In the playground I'm going to add *.fsx scripts created during learning process.

---
+++ Playground/GetDate.fsx
@@ -0,0 +1,20 @@
+// set up the vocabulary
+type DateScale = Hour | Hours | Day | Days | Week | Weeks
+type DateDirection = Ago | Hence
+
+// define a function that matches on the vocabulary
+let getDate interval scale direction =
+    let absHours = match scale with
+                   | Hour | Hours -> 1 * interval
+                   | Day | Days -> 24 * interval
+                   | Week | Weeks -> 24 * 7 * interval
+    let signedHours = match direction with
+                      | Ago -> -1 * absHours
+                      | Hence ->  absHours
+    System.DateTime.Now.AddHours(float signedHours)
+
+// test some examples
+let example1 = getDate 5 Days Ago
+let example2 = getDate 1 Hour Hence
+
+// To run select whole script and press "alt + enter", it runs in F# Interactive.
