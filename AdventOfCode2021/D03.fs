ec253176fa73e6e054ac6b532801224649610ded
Piotr Cieślik <piotr.cieslik@outlook.com>
2022-05-26 09:29:24 +0200

    Finish day 3 part 1

---
+++ AdventOfCode2021/D03.fs
@@ -0,0 +1,44 @@
+﻿module D03
+
+open System
+
+let input = System.IO.File.ReadAllLines("D03.txt")
+
+let part1 () =
+    let numberOfBits = input[0].Length
+    let initial = Array.replicate numberOfBits 0
+    
+    let sumOfBits =
+        input
+        |> Array.fold (fun state value ->
+                Seq.toArray value
+                |> Array.map (fun x -> string x)
+                |> Array.map (fun x -> int x)
+                |> Array.zip state
+                |> Array.map (fun (x, y) -> x + y)
+            ) initial
+
+    printfn "%A" sumOfBits
+    let treshhold = input.Length / 2
+    let gammaBits =
+        sumOfBits
+        |> Array.map (fun x -> if x > treshhold then 1 else 0)
+        |> Array.map (fun x -> x.ToString())
+        |> String.concat ""
+    let gammaBits32 = gammaBits.PadLeft ((32 - numberOfBits), '0')
+    let gamma = Convert.ToInt32(gammaBits32, 2)
+    printfn "gamma: %i" gamma
+
+    let epsilonBits =
+        sumOfBits
+        |> Array.map (fun x -> if x > treshhold then 0 else 1)
+        |> Array.map (fun x -> x.ToString())
+        |> String.concat ""
+    let epsilonBits32 = epsilonBits.PadLeft ((32 - numberOfBits), '0')
+    let epsilon = Convert.ToInt32(epsilonBits32, 2)
+    printfn "epsilon: %i" epsilon
+
+    gamma * epsilon
+
+let part2 () =
+    0
--- AdventOfCode2021/Program.fs
+++ AdventOfCode2021/Program.fs
@@ -11,3 +11,10 @@ printfn "Day 2, part 1: %i" d2p1
 
 let d2p2 = D02.part2()
 printfn "Day 2, part 2: %i" d2p2
+
+// Day 3
+let d3p1 = D03.part1()
+printfn "Day 3, part 1: %i" d3p1
+
+let d3p2 = D03.part2()
+printfn "Day 3, part 2: %i" d3p2
